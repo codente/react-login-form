a888c847f05997028114b0616fd6ec76ed26dae4
Jamie <jmeleacc@gmail.com>
2021-03-20 14:27:33 -0500

    deploy

---
+++ src/useForm.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+
+const useForm = (callback, validate) => {
+
+  const [values, setValues] = useState({});
+  const [errors, setErrors] = useState({});
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleSubmit = (event) => {
+	if (event) event.preventDefault();
+	setIsSubmitting(true)
+	 setErrors(validate(values))
+  };
+
+  const handleChange = (event) => {
+	event.persist();
+	setValues(values => ({ ...values, [event.target.name]: event.target.value }));
+  };
+
+  useEffect(() => {
+		if (Object.keys(errors).length === 0 && isSubmitting) {
+		  callback();
+		}
+	  }, [errors]);
+
+  return {
+	handleChange,
+	handleSubmit,
+	values,
+	errors
+  }
+};
+
+export default useForm;
